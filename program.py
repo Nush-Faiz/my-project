59ef8921bd7355215de34494bb632de323303b5a
Nush-Faiz <nushkiyafaiz15@gmail.com>
2025-03-25 15:09:43 +0530

    Adding fuel type in program.py

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 from car import Car
 
-car1 = Car('Toyota','Corolla',50)
-car2 = Car('Honda','Civic',40)
+car1 = Car('Toyota','Corolla',50, "Petrol")
+car2 = Car('Honda','Civic',40, "Diesel")
 
 print("Hello")
 car1.display_info()
